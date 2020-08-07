abf1571b2b27e321ab88488dd54dd75f51f66509
Jade <popkilj289@gmail.com>
2020-08-07 11:38:56 -0400

    Added uptime command, error handling and logging for disconnects

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 # Jadi3Pi run file
 # Imports
+from aiohttp.client_exceptions import ClientConnectorError
 from datetime import datetime
 import constants
 import discord
 import logger
+import time
 import os
 
 # Establishes logger
@@ -19,24 +21,37 @@ class JadieClient(discord.Client):
     # Keeps track of who we're copying by server and then id(hehehe)
     copied_users = {}
     
+    # Time since last disconnect.
+    bot_uptime = None
+    
+    # Keeps track of if this is the first time we've connected or not
+    connected_before = False
+    reconnected_since = False
+    
     
     # ===============================================================
     #                     GLOBAL BOT COMMANDS
     # ===============================================================
     def __init__(self):
+        """
+        Sets up the commands.
+        """
         discord.Client.__init__(self)
+        # Sets the command_dict!
         self.command_dict = {
+            'help': self.help_command,
             'runtime': self.runtime,
-            'copy': self.copy_user
+            'copy': self.copy_user,
+            'stopcopying': self.stop_copying,
+            'uptime': self.uptime
         }
     
     async def on_ready(self):
-        """Activates when client first links to Discord"""
+        """
+        Activates when client is ready for use on Discord (connected and ready)
+        """
         # Logs username and connection to discord
-        name_str = f'{self.user} has connected to Discord'
-        log.info('=' * (len(name_str) + 1))
-        log.info(name_str)
-        log.info('=' * (len(name_str) + 1))
+        log.info(f'{self.user} is ready')
         
         # Logs list of servers the bot is in
         if not self.guilds:
@@ -45,12 +60,39 @@ class JadieClient(discord.Client):
             for guild in self.guilds:
                 log.info(f'Active in guild "{guild.name}" with id {guild.id}')
         
-        # Spacing
-        log.info('')
+        # Bot uptime
+        # Bot uptime
+        self.bot_uptime = datetime.today()
+        
+    async def on_connect(self):
+        """
+        Whenever the bot connects to discord.
+        """
+        # Connected_before ensures that we don't log the first time we go through.
+        if self.connected_before:
+            log.info(f'{self.user} has reconnected to Discord')
+        else:
+            self.connected_before = True
+        # Reconnected_since makes sure we're not putting 2 disconnect messages in a row 
+        # Marks ONLY when a reconnect has happened since the last disconnect
+        self.reconnected_since = True
+        
+    async def on_disconnect(self):
+        """
+        Whenever the bot disconnects from discord.
+        """
+        # Reconnected_since makes sure we're not putting 2 disconnect messages in a row 
+        # Marks ONLY when a reconnect has happened since the last disconnect
+        if self.reconnected_since:
+            log.info(f'{self.user} has disconnected from Discord')
+            self.reconnected_since = False
+        
         
     async def on_message(self, message):
-        """Reacts to messages."""
-        if message.author == self.user:
+        """
+        Reacts to messages.
+        """
+        if not message or not message.content or not message.channel or not message.guild or not message.author or message.author == self.user:
             return
             
         # Reactive commands (will return True if method should return now)
@@ -75,44 +117,112 @@ class JadieClient(discord.Client):
     #                        FUN COMMANDS
     # ===============================================================
     async def copy_msg(self, message):
-        """Copies a user's message if they have been deemed COPIABLE by someone. 
-        That is, unless the message is to stop copying."""
+        """
+        Copies a user's message if they have been deemed COPIABLE by someone. 
+        That is, unless the message is to stop copying.
+        """
+        if message.content.startswith('j!stopcopying') and (len(message.content) == 13 or message.content[13] == ' '):
+            return
         if str(message.guild.id) in self.copied_users.keys() and message.author in self.copied_users[str(message.guild.id)]:
             await message.channel.send(message.content)
         
     async def copy_user(self, message, argument):
-        """Marks a user down as copiable -- so they will be copied in every message they send."""
+        """
+        Marks a user down as copiable.
+        Copiable users will be copied in every message they send.
+        """
         # Tries to get a valid user out of the argument.
         users = self.__get_closest_users(message, argument)
         if not users:
             await message.channel.send('Invalid user.')
-            log.info('User ' + str(message.author) + ' requested copy for user ' + argument + ', no idea who that is')
+            log.info(self.__get_comm_start + 'requested copy for user ' + argument + ', invalid user')
             return
         
         # Adds the users to the copy dict.
         if str(message.guild.id) not in self.copied_users.keys():
             self.copied_users.update({str(message.guild.id): []})
         for user in users:
-            await message.channel.send('Now copying user ' + user.nick)
+            
+            # Checks to make sure it isn't self.
+            if user == self.user:
+                if not len(users) - 1:
+                    log.debug(self.__get_comm_start + 'requested copy for THIS BOT. T_T')
+                    await message.channel.send('Yeah, no, I\'m not gonna copy myself.')
+                continue
+                
+            # Other.
+            await message.channel.send('Now copying user ' + (user.nick if user.nick else str(user)))
             if user not in self.copied_users[str(message.guild.id)]:
                 self.copied_users[str(message.guild.id)].append(user)
-                log.info('User ' + str(message.author) + ' requested copy for user ' + str(user) + ', now copying')
+                log.info(self.__get_comm_start + 'requested copy for user ' + str(user) + ', now copying')
             else:
-                log.info('User ' + str(message.author) + ' requested copy for user ' + str(user) + ', already copying')
+                log.info(self.__get_comm_start + 'requested copy for user ' + str(user) + ', already copying')
             
-            
+    async def stop_copying(self, message, argument):
+        """
+        Stops copying everyone in a server.
+        """
+        # If the thing exists in the dict
+        if str(message.guild.id) in self.copied_users.keys():
+            log.debug(self.__get_comm_start(message) + 'requested to stop copying people, {} users deleted from copied_users'.format(len(self.copied_users[str(message.guild.id)])))
+            self.copied_users.pop(str(message.guild.id))
+            await message.channel.send('No longer copying people in this server.')
+        else:
+            await message.channel.send('Wasn\'t copying anyone here to begin with, but ok.')
+            log.debug(self.__get_comm_start(message) + 'requested to stop copying people, already done')
     
     
     # ===============================================================
     #                      UTILITY COMMANDS
     # ===============================================================
+    async def help_command(self, message, argument):
+        """
+        Prints out the help response.
+        """
+        await message.channel.send(constants.HELP_MSG.format(constants.VERSION))
+        log.debug(self.__get_comm_start(message) + 'requested help message')
+        
     async def runtime(self, message, argument):
-        """Prints out the runtime of the bot"""
+        """
+        Prints out the runtime of the bot.
+        """
+        # Immediately returns if bot start time was not established
+        if not bot_start_time:
+            await message.channel.send('An error occurred while getting runtime, sorry! :P')
+            log.warning(self.__get_comm_start(message) + 'requested runtime, could not find start time')
+            return
+            
         # Time delta
         time_delta = datetime.today() - bot_start_time
         
-        # Get bot string
-        bot_str = constants.RUNTIME_PREFIX
+        bot_str = await self.report_time_passage(message, time_delta, constants.RUNTIME_PREFIX)
+        
+        # Logs message
+        log.debug(self.__get_comm_start(message) + 'requested runtime, responded with ' + bot_str[len(constants.RUNTIME_PREFIX):])
+        
+    async def uptime(self, message, argument):
+        """
+        Prints out the uptime of the bot.
+        """
+        # Immediately returns if bot start time was not established
+        if not self.bot_uptime:
+            await message.channel.send('An error occurred while getting uptime, sorry! :P')
+            log.warning(self.__get_comm_start(message) + 'requested runtime, could not find start time')
+            return
+            
+        # Time delta
+        time_delta = datetime.today() - self.bot_uptime
+        
+        # Does the thing
+        bot_str = await self.report_time_passage(message, time_delta, constants.UPTIME_PREFIX)
+        
+        # Logs message
+        log.debug(self.__get_comm_start(message) + 'requested uptime, responded with ' + bot_str[len(constants.UPTIME_PREFIX):])
+        
+    async def report_time_passage(self, message, time_delta, bot_str):
+        """
+        Creates the time delta string and reports to channel, then returns time delta string.
+        """
         if time_delta.days:
             bot_str+= str(time_delta.days) + 'd '
         if int(time_delta.seconds / 3600):
@@ -123,13 +233,13 @@ class JadieClient(discord.Client):
         
         # Send report
         await message.channel.send(bot_str)
-        # Logs message
-        log.info('User ' + str(message.author) + ' requested runtime, responded with ' + bot_str[len(constants.RUNTIME_PREFIX):])
+        
+        return bot_str
     
     
     # ===============================================================
     #               INTERNAL-USE (PRIVATE) COMMANDS
-    # ===============================================================
+    # ===============================================================    
     def __get_command_from_message(self, message):
         """Gets a command from a message, with one argument after"""
         # Immediately returns if command prefix is missing
@@ -152,8 +262,30 @@ class JadieClient(discord.Client):
             return message.mentions
         return []
         
+    @staticmethod
+    def __get_comm_start(message):
+        """Gets the command prefix. Just used to cut down space."""
+        return constants.COMM_LOG_PREFIX.format(message.author, message.channel, message.guild) 
+        
         
 # Client is the thing that is basically the connection between us and Discord -- time to run.
 if __name__ == '__main__':
     client = JadieClient()
-    client.run(constants.BOT_TOKEN)
+    
+    start_str = 'Starting new instance of JadieClient'
+    log.info('')
+    log.info('=' * (len(start_str) + 1))
+    log.info(start_str)
+    log.info('=' * (len(start_str) + 1))
+    
+    # All this crap around client.run occurs only if we can't connect initially.
+    try:
+        client.run(constants.BOT_TOKEN)
+        exit(0)
+    except ClientConnectorError:
+        log.info('Cannot connect to Discord, will attempt again in 3 minutes.')
+            
+        # Sleeps for 3 minutes so we don't overdo it.
+        time.sleep(180)
+        
+        exit(-1)
--- constants.py
+++ constants.py
@@ -1,4 +1,7 @@
 # Constants file
+# General use
+VERSION = '0.1.2'
+
 # Logging constants
 DEFAULT_LEVEL = 0
 LOG_TO_CONSOLE = True
@@ -6,9 +9,30 @@ LOG_TO_FILE = True
 DO_LEVEL_HEADERS = True
 DO_TIMESTAMPS = True
 LOGS_DIR = 'logs'
+# Server, channel, user
+COMM_LOG_PREFIX = '{} ({}, {}): '
 
 # Bot stuff
 BOT_TOKEN = 'NzQwNjEwMTc2MDM3NTUyMTc4.Xyrg-Q.8jBguEMXHXFmZvXOY5UAdXu4FE0'
 GLOBAL_PREFIX = 'j!'
 
 RUNTIME_PREFIX = 'Bot has been running for '
+UPTIME_PREFIX = 'Bot has been connected for '
+
+HELP_MSG ='''```
+Jadi3Pi {} Help
+PREFIX: j!
+
+===============
+      FUN
+===============
+copy: Mention someone to start copying their every word
+stopcopying: Stop copying everyone in this server
+
+===============
+    UTILITY
+===============
+help: Display this message
+runtime: Display the amount of time this bot has been running for
+uptime: Display the amount of time this bot has been connected to Discord for
+```'''
