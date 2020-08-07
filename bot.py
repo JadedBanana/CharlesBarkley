257c85a2c2e2ea9d7199b231f664a54dae63f543
Jade <popkilj289@gmail.com>
2020-08-07 18:47:56 -0400

    Added base conversion and random youtube link generation

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 # Jadi3Pi run file
 # Imports
-from aiohttp.client_exceptions import ClientConnectorError
 from datetime import datetime
+from urllib import request
 import constants
 import discord
 import logger
+import random
+import string
+import json
 import time
 import os
 
@@ -28,6 +31,9 @@ class JadieClient(discord.Client):
     connected_before = False
     reconnected_since = False
     
+    # Prefixes for bases that aren't decimal
+    nondecimal_bases = {'0x': [16, 'hexadecimal'], '0d': [12, 'duodecimal'], '0o': [8, 'octal'], '0b': [2, 'binary']}    
+    
     
     # ===============================================================
     #                     GLOBAL BOT COMMANDS
@@ -43,7 +49,22 @@ class JadieClient(discord.Client):
             'runtime': self.runtime,
             'copy': self.copy_user,
             'stopcopying': self.stop_copying,
-            'uptime': self.uptime
+            'uptime': self.uptime,
+            'hex': self.hexadecimal,
+            'hexadecimal': self.hexadecimal,
+            'duo': self.duodecimal,
+            'duodec': self.duodecimal,
+            'duodecimal': self.duodecimal,
+            'dec': self.decimal,
+            'decimal': self.decimal,
+            'oct': self.octal,
+            'octal': self.octal,
+            'bin': self.binary,
+            'binary': self.binary,
+            'randomyt': self.randomyt,
+            'randomyoutube': self.randomyt,
+            'ytroulette': self.randomyt,
+            'youtuberoulette': self.randomyt
         }
     
     async def on_ready(self):
@@ -60,7 +81,6 @@ class JadieClient(discord.Client):
             for guild in self.guilds:
                 log.info(f'Active in guild "{guild.name}" with id {guild.id}')
         
-        # Bot uptime
         # Bot uptime
         self.bot_uptime = datetime.today()
         
@@ -86,7 +106,6 @@ class JadieClient(discord.Client):
         if self.reconnected_since:
             log.info(f'{self.user} has disconnected from Discord')
             self.reconnected_since = False
-        
         
     async def on_message(self, message):
         """
@@ -171,9 +190,112 @@ class JadieClient(discord.Client):
             await message.channel.send('Wasn\'t copying anyone here to begin with, but ok.')
             log.debug(self.__get_comm_start(message) + 'requested to stop copying people, already done')
     
+    async def randomyt(self, message, argument):
+        """Generates a random youtube link."""
+        search_results = {'items': []}
+        while not search_results['items']:
+            # Gets random search term and searches
+            random_search = ''.join(random.choice(string.ascii_uppercase + string.digits) for i in range(random.choices(constants.YOUTUBE_SEARCH_LENGTHS, weights=constants.YOUTUBE_SEARCH_WEIGHTS)[0]))
+            url_data = constants.YOUTUBE_SEARCH_URL.format(constants.YOUTUBE_API_KEY, constants.YOUTUBE_SEARCH_COUNT, random_search)
+            
+            # Opens url
+            url_data = request.urlopen(url_data)
+            data = url_data.read()
+            
+            # Decodes data and makes it into a dict
+            encoding = url_data.info().get_content_charset('utf-8')
+            search_results = json.loads(data.decode(encoding))
+            
+        # Create list of video id's
+        video_ids = [video['id']['videoId'] for video in search_results['items']]
+        
+        # Pick one
+        choice = random.randint(0, len(video_ids) - 1)
+        
+        # Return selected video.
+        await message.channel.send(constants.YOUTUBE_VIDEO_URL_FORMAT.format(video_ids[choice]))
+        log.debug(self.__get_comm_start(message) + 'requested random video, returned video id ' + video_ids[choice] + ' which was result number ' + str(choice) + ' in the results for ' + random_search)
+        
     
     # ===============================================================
     #                      UTILITY COMMANDS
+    # ===============================================================
+    async def hexadecimal(self, message, argument):
+        """Converts a number to hexadecimal"""
+        # Getting the number
+        num = await self.__get_num_from_argument(message, argument)
+        
+        # Error handling for not numbers
+        if isinstance(num, str):
+            log.debug(self.__get_comm_start(message) + 'requested hex conversion for number {}, invalid'.format(argument))
+            return
+            
+        num = self.__convert_num_from_decimal(num, 16)
+        
+        await message.channel.send('0x' + str(num))
+        log.debug(self.__get_comm_start(message) + 'requested hex conversion for number {}, responded with 0x{}'.format(argument, num))
+        
+    async def duodecimal(self, message, argument):
+        """Converts a number to duodecimal"""
+        # Getting the number
+        num = await self.__get_num_from_argument(message, argument)
+        
+        # Error handling for not numbers
+        if isinstance(num, str):
+            log.debug(self.__get_comm_start(message) + 'requested duodec conversion for number {}, invalid'.format(argument))
+            return
+            
+        num = self.__convert_num_from_decimal(num, 12)
+        
+        await message.channel.send('0d' + str(num))
+        log.debug(self.__get_comm_start(message) + 'requested duodec conversion for number {}, responded with 0d{}'.format(argument, num))
+        
+    async def decimal(self, message, argument):
+        """Converts a number to decimal."""
+        # Getting the number
+        num = await self.__get_num_from_argument(message, argument)
+        
+        # Error handling for not numbers
+        if isinstance(num, str):
+            log.debug(self.__get_comm_start(message) + 'requested decimal conversion for number {}, invalid'.format(argument))
+            return
+            
+        await message.channel.send(int(num) if num % 1 == 0 else num)
+        log.debug(self.__get_comm_start(message) + 'requested decimal conversion for number {}, responded with {}'.format(argument, int(num) if num % 1 == 0 else num))
+        
+    async def octal(self, message, argument):
+        """Converts a number to octal"""
+        # Getting the number
+        num = await self.__get_num_from_argument(message, argument)
+        
+        # Error handling for not numbers
+        if isinstance(num, str):
+            log.debug(self.__get_comm_start(message) + 'requested octal conversion for number {}, invalid'.format(argument))
+            return
+            
+        num = self.__convert_num_from_decimal(num, 8)
+        
+        await message.channel.send('0o' + str(num))
+        log.debug(self.__get_comm_start(message) + 'requested octal conversion for number {}, responded with 0o{}'.format(argument, num))
+        
+    async def binary(self, message, argument):
+        """Converts a number to binary"""
+        # Getting the number
+        num = await self.__get_num_from_argument(message, argument)
+        
+        # Error handling for not numbers
+        if isinstance(num, str):
+            log.debug(self.__get_comm_start(message) + 'requested binary conversion for number {}, invalid'.format(argument))
+            return
+            
+        num = self.__convert_num_from_decimal(num, 2)
+        
+        await message.channel.send('0b' + str(num))
+        log.debug(self.__get_comm_start(message) + 'requested binary conversion for number {}, responded with 0b{}'.format(argument, num))
+    
+    
+    # ===============================================================
+    #                       OTHER COMMANDS
     # ===============================================================
     async def help_command(self, message, argument):
         """
@@ -195,7 +317,7 @@ class JadieClient(discord.Client):
         # Time delta
         time_delta = datetime.today() - bot_start_time
         
-        bot_str = await self.report_time_passage(message, time_delta, constants.RUNTIME_PREFIX)
+        bot_str = await self.__report_time_passage(message, time_delta, constants.RUNTIME_PREFIX)
         
         # Logs message
         log.debug(self.__get_comm_start(message) + 'requested runtime, responded with ' + bot_str[len(constants.RUNTIME_PREFIX):])
@@ -214,27 +336,10 @@ class JadieClient(discord.Client):
         time_delta = datetime.today() - self.bot_uptime
         
         # Does the thing
-        bot_str = await self.report_time_passage(message, time_delta, constants.UPTIME_PREFIX)
+        bot_str = await self.__report_time_passage(message, time_delta, constants.UPTIME_PREFIX)
         
         # Logs message
         log.debug(self.__get_comm_start(message) + 'requested uptime, responded with ' + bot_str[len(constants.UPTIME_PREFIX):])
-        
-    async def report_time_passage(self, message, time_delta, bot_str):
-        """
-        Creates the time delta string and reports to channel, then returns time delta string.
-        """
-        if time_delta.days:
-            bot_str+= str(time_delta.days) + 'd '
-        if int(time_delta.seconds / 3600):
-            bot_str+= str(int(time_delta.seconds / 3600)) + 'h '
-        if int(time_delta.seconds / 60):
-            bot_str+= str(int(time_delta.seconds % 3600 / 60)) + 'm '
-        bot_str+= str(time_delta.seconds % 60) + 's '
-        
-        # Send report
-        await message.channel.send(bot_str)
-        
-        return bot_str
     
     
     # ===============================================================
@@ -262,16 +367,131 @@ class JadieClient(discord.Client):
             return message.mentions
         return []
         
+    async def __report_time_passage(self, message, time_delta, bot_str):
+        """
+        Creates the time delta string and reports to channel, then returns time delta string.
+        """
+        if time_delta.days:
+            bot_str+= str(time_delta.days) + 'd '
+        if int(time_delta.seconds / 3600):
+            bot_str+= str(int(time_delta.seconds / 3600)) + 'h '
+        if int(time_delta.seconds / 60):
+            bot_str+= str(int(time_delta.seconds % 3600 / 60)) + 'm '
+        bot_str+= str(time_delta.seconds % 60) + 's '
+        
+        # Send report
+        await message.channel.send(bot_str)
+        
+        return bot_str
+    
+    @staticmethod
+    def __normalize_string(string, remove_double_spaces=True):
+        """Removes spaces at the start and end as well as double spaces in a string."""
+        # Start spaces
+        while string.startswith(' '):
+            string = string[1:]
+        # End spaces
+        while string.endswith(' '):
+            string = string[:len(string) - 1]
+        # Double spaces
+        if remove_double_spaces:
+            while '  ' in string:
+                string = string.replace('  ', ' ')
+        # Return
+        return string
+    
     @staticmethod
     def __get_comm_start(message):
         """Gets the command prefix. Just used to cut down space."""
-        return constants.COMM_LOG_PREFIX.format(message.author, message.channel, message.guild) 
+        return constants.COMM_LOG_PREFIX.format(message.author, message.channel, message.guild)
+    
+    async def __get_num_from_argument(self, message, argument):
+        # Gets usages for arguments
+        argument = self.__normalize_string(argument)
+        argument2 = argument.lower()
         
+        # Nondecimal bases
+        for base in self.nondecimal_bases.keys():
+            if argument2.startswith(base):
+                try:
+                    return self.__convert_num_to_decimal(argument[2:], self.nondecimal_bases[base][0])
+                except ValueError:
+                    await message.channel.send(argument + ' is not a valid {} number').format(self.nondecimal_bases[base][1])
+                    return ''
         
+        # Decimal base
+        try:
+            return float(argument)
+        except ValueError:
+            await message.channel.send(argument + ' is not a valid decimal number')
+            return ''
+    
+    @staticmethod
+    def __convert_num_to_decimal(n, base):
+        """Converts a number to decimal from another base."""
+        # If there's more than 1 decimal point, we raise a ValueError.
+        if n.count('.') > 1:
+            raise ValueError()
+            
+        # If there is no decimal point, we take the easy way out.
+        if '.' not in n:
+            return int(n, base=base)
+        
+        # Otherwise, we're in for a ride.
+        else:
+            # Get the index of the period.
+            per_index = n.find('.')
+            
+            # Take the easy way for the numbers BEFORE the decimal point.
+            final_num = int(n[:per_index], base=base)
+            
+            # We add a little leniency for those below base 36 in terms of capitalization.
+            if base <= 36:
+                n = n.upper()
+            
+            # Do it ourselves for numbers after.
+            for index in range(per_index + 1, len(n)):
+                exp = per_index - index
+                num_of = constants.CONVERT_CHARS.find(n[index])
+                # A little error handling for -1's or stuff outside the range.
+                if num_of == -1 or num_of >= base:
+                    raise ValueError()
+                else:
+                    final_num+= base**exp * num_of
+                
+            return final_num
+    
+    @staticmethod
+    def __convert_num_from_decimal(n, base):
+        """Converts a number from decimal to another base."""
+        # Gets maximum exponent of base
+        exp = 0
+        while base**(exp  + 1) <= n:
+            exp+= 1
+        
+        # Adds all the numbers that aren't a multiple of base to the string.
+        num_str = ''
+        while n != 0 and exp >= constants.MAX_CONVERT_DEPTH:
+            # Adds a decimal point if we're below 0 exp.
+            if exp == -1:
+                num_str+= '.'
+            num_str+= constants.CONVERT_CHARS[int(n / base**exp)]
+            n -= (int(n / base**exp) * base**exp)
+            exp-= 1
+            
+        # Adds all the zeros between exp and 0 if exp is not below 0.
+        while exp >= 0:
+            num_str+= '0'
+            exp-= 1
+            
+        return num_str
+            
+    
 # Client is the thing that is basically the connection between us and Discord -- time to run.
 if __name__ == '__main__':
     client = JadieClient()
     
+    # Logging new instance
     start_str = 'Starting new instance of JadieClient'
     log.info('')
     log.info('=' * (len(start_str) + 1))
--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 # Constants file
 # General use
-VERSION = '0.1.2'
+VERSION = '0.1.4'
 
 # Logging constants
 DEFAULT_LEVEL = 0
@@ -16,6 +16,16 @@ COMM_LOG_PREFIX = '{} ({}, {}): '
 BOT_TOKEN = 'NzQwNjEwMTc2MDM3NTUyMTc4.Xyrg-Q.8jBguEMXHXFmZvXOY5UAdXu4FE0'
 GLOBAL_PREFIX = 'j!'
 
+YOUTUBE_API_KEY = 'AIzaSyDfdrytJZ4Ft9W76VdR-t4HOZTOR7ESMEg'
+YOUTUBE_VIDEO_URL_FORMAT = 'https://www.youtube.com/watch?v={}'
+YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search?key={}&maxResults={}&part=snippet&type=video&q={}'
+YOUTUBE_SEARCH_COUNT = 100
+YOUTUBE_SEARCH_LENGTHS = [1, 2, 3, 4, 5]
+YOUTUBE_SEARCH_WEIGHTS = [36, 1296, 46656, 1679616, 60466176]
+
+CONVERT_CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+_'
+MAX_CONVERT_DEPTH = -16
+
 RUNTIME_PREFIX = 'Bot has been running for '
 UPTIME_PREFIX = 'Bot has been connected for '
 
@@ -28,9 +38,19 @@ PREFIX: j!
 ===============
 copy: Mention someone to start copying their every word
 stopcopying: Stop copying everyone in this server
+randomyt / randomyoutube: Generate a random YouTube video
 
 ===============
     UTILITY
+===============
+hex / hexadecimal: Converts a number to hexadecimal
+duo / duodec / duodecimal: Converts a number to duodecimal
+dec / decimal: Converts a number to decimal
+oct / octal: Converts a number to octal
+bin / binary: Converts a number to binary
+
+===============
+     OTHER
 ===============
 help: Display this message
 runtime: Display the amount of time this bot has been running for
