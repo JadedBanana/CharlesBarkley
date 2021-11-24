8ddd04ffe1557e17edd718ae6921b07aa27bf3b0
popki <popkilj289@gmail.com>
2021-11-24 18:46:19 -0500

    Coding convention changes

--- lib/util/exceptions.py
+++ lib/util/exceptions.py
@@ -3,36 +3,40 @@ class InvalidDotenvFileError(ValueError):
     def __init__(self, reason):
         self.strerr = reason
 
+
 # UndefinedVariableError is raised when variables in .env file are defined incorrectly or not at all.
 class UndefinedVariableError(ValueError):
     def __init__(self, reason):
         self.strerr = reason
+
 
 # DuplicateCommandError is raised when the command loader finds two commands using the same command name.
 class DuplicateCommandError(ValueError):
     def __init__(self, reason):
         self.strerr = reason
 
-# FirstMessageInChannelError is raised when attempting to pull the previous message fails because this is the first message in the channel.
+
+# FirstMessageInChannelError is raised when attempting to pull the previous message fails because this is the first
+# message in the channel.
 class FirstMessageInChannelError(Exception):
     def __init__(self):
         pass
+
 
 # CannotAccessUserlistError is raised when can't access the userlist.
 class CannotAccessUserlistError(Exception):
     def __init__(self):
         pass
 
+
 # NoUserSpecifiedError is raised when trying to access a user from an argument that it can't find.
 class NoUserSpecifiedError(Exception):
     def __init__(self):
         pass
+
 
 # UnableToFindUserError is raised when can't find a user based on that argument.
 class UnableToFindUserError(Exception):
     def __init__(self, completed_users, incomplete_user):
         self.completed_users = completed_users
         self.incomplete_user = incomplete_user
-
-
-
