42c13d1ef1e5cb1ff781ef62e3a4528dba45b412
popki <popkilj289@gmail.com>
2021-11-26 15:21:08 -0500

    Added secret 'swooce' command (hehe)

---
+++ lib/commands/fun/swooce.py
@@ -0,0 +1,31 @@
+"""
+Swooce command.
+Watch me SWOOCE right in! Swooce... swooce!
+"""
+# Imports
+from lib.util.logger import BotLogger as logging
+from lib.util import messaging
+
+
+# Swooce YouTube url.
+SWOOCE = 'https://www.youtube.com/watch?v=gghBcjgOdd0'
+
+
+async def swooce(bot, message, argument):
+    """
+    Sends the swooce video.
+
+    Arguments:
+        bot (lib.bot.JadieClient) : The bot object that called this command.
+        message (discord.message.Message) : The discord message object that triggered this command.
+        argument (str) : The command's argument, if any.
+    """
+    # Log and send.
+    logging.info(message, 'swooced right in')
+    await messaging.send_text_message(message, SWOOCE)
+
+
+# Command values
+PUBLIC_COMMAND_DICT = {
+    'swooce': swooce
+}
