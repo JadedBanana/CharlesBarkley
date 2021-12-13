5fadc0ba72387e090342bab69ff735254bfb47b6
popki <popkilj289@gmail.com>
2021-12-13 02:12:33 -0500

    Renamed active_profile_pictures.py to active_pfps.py

--- lib/commands/dev/active_pfps.py
+++ lib/commands/dev/active_pfps.py
@@ -17,14 +17,16 @@ async def active_pfps(bot, message, argument):
         argument (str) : The command's argument, if any.
     """
     # If there are no active profile pictures, then just say that there's no active profile pictures.
-    if not tempfiles.ACTIVE_PROFILE_PICTURES:
+    if not temp_files.ACTIVE_PROFILE_PICTURES:
         logging.info(message, 'requested active profile pictures, but there are none')
         return await messaging.send_text_message(message, 'No active profile pictures.')
 
     # Active profile pictures found, send the codeblock.
-    logging.info(message, f'requested active profile pictures, sent back {len(tempfiles.ACTIVE_PROFILE_PICTURES)} entries')
-    await messaging.send_codeblock_message(message, '\n'.join([f'{pfp_id}: {", ".join(key for key in pfp_tuple[1])}'
-                                                               for pfp_id, pfp_tuple in tempfiles.ACTIVE_PROFILE_PICTURES.items()]))
+    logging.info(message, f'requested active profile pictures, sent back '
+                          f'{len(temp_files.ACTIVE_PROFILE_PICTURES)} entries')
+    await messaging.send_codeblock_message(message, '\n'.join(
+        [f'{pfp_id}: {", ".join(key for key in pfp_tuple[1])}'
+         for pfp_id, pfp_tuple in temp_files.ACTIVE_PROFILE_PICTURES.items()]))
 
 
 # Command values
