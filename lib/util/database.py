ac1cdf61e5ec5fe40c780ecc6d17ee08885d1e25
popki <popkilj289@gmail.com>
2022-01-02 20:41:21 -0500

    Overhauled hunger games generation to take full advantage of database

--- lib/util/database.py
+++ lib/util/database.py
@@ -14,15 +14,15 @@ import sys
 EXPECTED_DATABASE_TABLES = {
     'reminders': 'REMINDER_TABLE',
     'hg_phases': 'HG_PHASES_TABLE',
-    'hg_actions': 'HG_ACTION_TABLE',
-    'hg_action_wrappers': 'HG_ACTION_WRAPPER_TABLE',
+    'hg_actions': 'HG_ACTIONS_TABLE',
+    'hg_action_wrappers': 'HG_ACTION_WRAPPERS_TABLE',
     'hg_current_game_phases': 'HG_CURRENT_GAME_PHASES_TABLE',
     'hg_current_game_actions': 'HG_CURRENT_GAME_ACTIONS_TABLE'
 }
 
 # List of commands that use specific database tables.
 COMMANDS_USING_DATABASE_TABLES = {
-    'hungergames': ('HG_PHASES_TABLE', 'HG_ACTION_TABLE', 'HG_ACTION_WRAPPER_TABLE', 'HG_CURRENT_GAME_PHASES_TABLE',
+    'hungergames': ('HG_PHASES_TABLE', 'HG_ACTIONS_TABLE', 'HG_ACTION_WRAPPERS_TABLE', 'HG_CURRENT_GAME_PHASES_TABLE',
                     'HG_CURRENT_GAME_ACTIONS_TABLE'),
     'remindme': ('REMINDER_TABLE',)
 }
@@ -31,6 +31,181 @@ COMMANDS_USING_DATABASE_TABLES = {
 BASE = None
 ENGINE = None
 SESSION = None
+
+
+def get_all(table):
+    """
+    Gets every row from the given table.
+
+    Args:
+        database table : Which table to pull from.
+
+    Returns:
+        SQLAlchemy.Query : The finalized query.
+    """
+    # Simple execution.
+    return SESSION.query(table)
+
+
+def get_filtered(table, **kwargs):
+    """
+    Gets a filtered set of rows from the given table according to the kwargs.
+
+    Args:
+        database table : Which table to pull from.
+        **kwargs : All variables from which to filter.
+                   Must refer to database columns equaling something else.
+
+    Returns:
+        SQLAlchemy.Query : The finalized query.
+    """
+    # Simple execution.
+    return filter(SESSION.query(table), **kwargs)
+
+
+def get_all_joined(base_table, *tables_and_ons):
+    """
+    Gets a filtered set of rows from the given base_table, combined with the rest of the tables referenced above,
+    according to the provided kwargs.
+
+    Args:
+        base_table (database table) : The starting table to pull from.
+        *tables_and_ons (database table, database column equality)[] : Tuples whose first item is the table to pull
+                                                                       from, and whose second item is the equality on
+                                                                       which to base the inner join.
+
+    Returns:
+        SQLAlchemy.Query : The finalized query.
+    """
+    # Create the query step-by-step, starting with the base table.
+    base_query = join(SESSION.query(base_table), tables_and_ons)
+
+    # Return.
+    return base_query
+
+
+def get_filtered_joined(base_table, *tables_and_ons, **kwargs):
+    """
+    Gets every row from the given base_table, combined with the rest of the tables referenced above.
+
+    Args:
+        base_table (database table) : The starting table to pull from.
+        *tables_and_ons (database table, database column equality)[] : Tuples whose first item is the table to pull
+                                                                       from, and whose second item is the equality on
+                                                                       which to base the inner join.
+        **kwargs : All variables from which to filter.
+                   Must refer to database columns equaling something else.
+
+    Returns:
+        SQLAlchemy.Query : The finalized query.
+    """
+    # Simple return.
+    return filter(join(SESSION.query(base_table), *tables_and_ons), **kwargs)
+
+    # Create the query step-by-step, starting with the base table.
+    base_query = SESSION.query(base_table)
+
+    # Iterate through all tables_and_ons and add them slowly.
+    for table_and_on in tables_and_ons:
+        base_query = base_query.join(table_and_on[0], table_and_on[1])
+
+    # Filter.
+    base_query = base_query.filter_by(**kwargs)
+
+    # Return.
+    return base_query
+
+
+def get_filtered_joined(base_table, *tables_and_ons, **kwargs):
+    """
+    Gets every row from the given base_table, combined with the rest of the tables referenced above.
+
+    Args:
+        base_table (database table) : The starting table to pull from.
+        *tables_and_ons (database table, database column equality)[] : Tuples whose first item is the table to pull
+                                                                       from, and whose second item is the equality on
+                                                                       which to base the inner join.
+        **kwargs : All variables from which to filter.
+                   Must refer to database columns equaling something else.
+
+    Returns:
+        SQLAlchemy.Query : The finalized query.
+    """
+    # Simple return.
+    return filter(join(SESSION.query(base_table), *tables_and_ons), **kwargs)
+
+
+def filter(base_query, **kwargs):
+    """
+    Filters the given query according to the kwargs.
+
+    Args:
+        base_query (SQLAlchemy.Query) : The starting query to add onto.
+        **kwargs : All variables from which to filter.
+                   Must refer to database columns equaling something else.
+
+    Returns:
+        SQLAlchemy.Query : The finalized query.
+    """
+    # Simple return.
+    return base_query.filter_by(**kwargs)
+
+
+def join(base_query, *tables_and_ons):
+    """
+    Joins the given tables onto the base_query according to the ON statements.
+
+    Args:
+        base_query (SQLAlchemy.Query) : The starting query to add onto.
+        *tables_and_ons (database table, database column equality)[] : Tuples whose first item is the table to pull
+                                                                       from, and whose second item is the equality on
+                                                                       which to base the inner join.
+
+    Returns:
+        SQLAlchemy.Query : The finalized query.
+    """
+    # Create new_query from base_query.
+    new_query = base_query
+
+    # Iterate through all tables_and_ons and add them slowly.
+    for table_and_on in tables_and_ons:
+        new_query = new_query.join(table_and_on[0], table_and_on[1])
+
+    # Return.
+    return new_query
+
+
+def insert_into_database(table, **kwargs):
+    """
+    Creates and inserts a new object into the database.
+
+    Args:
+        table (database table) : The starting table to instantiate the object from.
+        **kwargs : All the constructor arguments.
+
+    Returns:
+        new_object (database table object) : The object that was inserted.
+    """
+    # Create the object.
+    table_object = table(**kwargs)
+
+    # Commit.
+    commit_to_database(table_object)
+
+    # Return.
+    return table_object
+
+
+def commit_to_database(new_object):
+    """
+    Commits a new object to the database.
+
+    Arguments:
+        new_object (database table object) : The object to insert into the database.
+    """
+    # Add and commit.
+    SESSION.add(new_object)
+    SESSION.commit()
 
 
 def initialize():
@@ -61,7 +236,6 @@ def initialize():
         logging.error('Error occurred during database mapping.')
         import traceback
         logging.error(traceback.format_exc().replace('\n\n', '\n').strip('\n'))
-
 
         # If we are supposed to exit, exit.
         if environment.get('EXIT_ON_DATABASE_FAILURE'):
