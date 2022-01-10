000b93f0e04b9ef3177b4ec7192b713c3b982ac5
popki <popkilj289@gmail.com>
2022-01-10 11:51:32 -0500

    Made uno able to paste transparent images correctly

--- lib/commands/fun_interactive/uno.py
+++ lib/commands/fun_interactive/uno.py
@@ -4,7 +4,8 @@ Recreates the Mattel Game UNO inside the bot.
 Mattel, pls don't sue.
 """
 # Local Imports
-from lib.util import arguments, assets, database, discord_info, environment, messaging, misc, parsing, tasks, temp_files
+from lib.util import arguments, assets, database, discord_info, environment, graphics, messaging, misc, parsing, \
+    tasks, temp_files
 from lib.util.exceptions import CannotAccessUserlistError, InvalidHungerGamesPhaseError, NoUserSpecifiedError, \
     UnableToFindUserError
 from lib.commands import fun_interactive as game_manager
@@ -16,18 +17,34 @@ from PIL import Image, ImageOps, ImageDraw
 from datetime import datetime
 import discord
 import random
+import os
 
 
 # Keeps track of current games.
 CURRENT_GAMES = {}
 
+# Game generation
+MAX_GAMESIZE = 10
+MIN_GAMESIZE = 2
+DEFAULT_GAMESIZE = 10
+
 # Embeds.
-UNO_EMBED_COLOR = (203 << 16) + (1 << 8)
+EMBED_COLOR = (203 << 16) + (1 << 8)
+
+# Graphics data.
+LOBBY_WIDTH = 900
+CARD_IMAGE_SIZE = (192, 290)
+CARD_IMAGE_TYPE = '.png'
 
 # Pregame
-UNO_LOBBY_TITLE = 'Who\'s up for a game of UNO?'
-UNO_LOBBY_BACKGROUND_IMAGE = 'cards/backgrounds/uno_lobby.png'
-UNO_LOBBY_FAILSAFE_BACKGROUND = (203, 1, 0)
+LOBBY_TITLE = 'Who\'s up for a game of UNO?'
+LOBBY_BACKGROUND_IMAGE = 'cards/backgrounds/uno_lobby.png'
+LOBBY_FAILSAFE_BACKGROUND = (203, 1, 0)
+LOBBY_CARD_COLOR_DEFAULT = 'blank'
+LOBBY_CARD_COLORS = ['red', 'blue', 'green', 'yellow']
+LOBBY_CARD_PFP_SIZE = (151, 151)
+LOBBY_CARD_PFP_OFFSET = (21, 70)
+LOBBY_CARD_DIRECTORY = 'cards/uno/lobby'
 
 # Cards.
 CARDS = [
@@ -104,8 +121,23 @@ async def uno_start(message, argument):
         logging.debug(message, 'requested Uno, but other game active')
         return await game_manager.send_game_in_progress_message(message)
 
+    # Otherwise, we instantiate a game.
+    # Gets argument for how many users to start uno with.
+    if argument:
+        try:
+            # Get a number from the argument.
+            player_count = int(parsing.normalize_string(argument))
+        # If the conversion doesn't work, use the default.
+        except ValueError:
+            player_count = DEFAULT_GAMESIZE
+    # No argument, use the default player count.
+    else:
+        player_count = DEFAULT_GAMESIZE
+
     # Generate the uno dict.
-    uno_dict = {'past_pregame': False, 'updated': datetime.today(), 'host': message.author}
+    uno_dict = {'past_pregame': False, 'updated': datetime.today(), 'host': message.author, 'players': [message.author],
+                'lobby_colors': [random.randint(0, 3)] + [-1 for i in range(player_count - 1)],
+                'readies': [False for i in range(player_count)]}
     CURRENT_GAMES[uno_key] = uno_dict
 
     # Checkout the host's profile picture.
@@ -119,7 +151,7 @@ async def uno_start(message, argument):
     logging.debug(message, f'started Uno instance')
 
 
-async def send_pregame(message, uno_dict, title=UNO_LOBBY_TITLE):
+async def send_pregame(message, uno_dict, title=LOBBY_TITLE):
     """
     Sends the pregame lobby thing.
 
@@ -132,7 +164,7 @@ async def send_pregame(message, uno_dict, title=UNO_LOBBY_TITLE):
     image = makeimage_lobby(uno_dict)
 
     # Sends image, logs.
-    await messaging.send_image_based_embed(message, image, title, UNO_EMBED_COLOR,
+    await messaging.send_image_based_embed(message, image, title, EMBED_COLOR,
                                            description=f"Hosted by {uno_dict['host'].display_name}")
 
 
@@ -144,17 +176,45 @@ def makeimage_lobby(uno_dict):
         uno_dict (dict) : The full game dict.
 
     Returns:
-        PIL.Image : The finalized image.
+        PIL.Image.Image : The finalized image.
     """
     # Make the lobby image.
-    lobby_image = Image.new('RGB', (900, 600), UNO_LOBBY_FAILSAFE_BACKGROUND)
+    lobby_image = Image.new('RGBA', (900, 600), LOBBY_FAILSAFE_BACKGROUND)
 
     # Get the lobby background image and paste it onto the lobby image.
-    lobby_background = assets.open_image(UNO_LOBBY_BACKGROUND_IMAGE)
+    lobby_background = assets.open_image(LOBBY_BACKGROUND_IMAGE)
     lobby_image.paste(lobby_background, (0, int((600 - lobby_background.size[1]) / 2)))
+
+    card_image = makeimage_lobby_card(uno_dict['host'], 1, 2)
+    lobby_image = graphics.transparency_paste(lobby_image, card_image, (300, 300))
 
     # Return the lobby image.
     return lobby_image
+
+
+def makeimage_lobby_card(player, card_index, card_color):
+    """
+    Generates a lobby card for the given player and returns it.
+    If the player is None, then a blank one is generated instead.
+
+    Args:
+        player (discord.User) : The player object that the card is for.
+                                CAN be None.
+        card_index (int) : The index of the card.
+                           Must be between 0 and 9, inclusive.
+        card_color (int) : The color of the card.
+                           Must be between 0 and 3, inclusive.
+
+    Returns:
+        PIL.Image.Image : The finalized image.
+    """
+    # Load up the original card image.
+    card_image_name = \
+        f'{card_index}_{LOBBY_CARD_COLORS[card_color] if player else LOBBY_CARD_COLOR_DEFAULT}{CARD_IMAGE_TYPE}'
+    card_image = assets.open_image(os.path.join(LOBBY_CARD_DIRECTORY, card_image_name))
+
+    return card_image
+
 
 
 def initialize(bot):
