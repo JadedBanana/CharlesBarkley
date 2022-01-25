b9642ab3bf415b00a209a74f23e34c6592ec910d
popki <popkilj289@gmail.com>
2022-01-25 15:27:05 -0500

    Removed b.py

--- b.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-for color in ['red', 'blue', 'green', 'yellow']:
-    for i in range(10):
-        subprocess.run(['mv', f'assets/cards/uno/lobby/{color}_{i}.png', f'assets/cards/uno/lobby/{i}_{color}.png'])
